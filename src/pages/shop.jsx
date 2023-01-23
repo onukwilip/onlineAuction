152f2f843c65262c94492b7efedbcceedb1385c8
memmcol-prince <memmcol.prince@gmail.com>
2023-01-23 17:50:33 +0100

    Added Footer component to Shop page

--- src/pages/shop.jsx
+++ src/pages/shop.jsx
@@ -14,6 +14,7 @@ import {
   Input,
 } from "semantic-ui-react";
 import { useTimer } from "react-timer-hook";
+import Footer from "@/components/Footer";
 
 class Category {
   constructor(name) {
@@ -250,6 +251,7 @@ const Shop = () => {
           </div>
         )}
       </div>
+      <Footer />
     </div>
   );
 };
