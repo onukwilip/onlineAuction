6b4adc51b463387d9ac5238f5d19321e95521880
onukwilip <onukwilip@gmail.com>
2023-06-24 16:07:24 +0100

    Added redis url for deployment

--- src/config/redis-config.js
+++ src/config/redis-config.js
@@ -1,8 +1,7 @@
 import { createClient } from "redis";
 
 export default function () {
-  // const client = createClient({ url: process.env.REDIS_URL });
-  const client = createClient();
+  const client = createClient({ url: process.env.REDIS_URL });
   client.on("error", (e) => console.log("An error occured ", e));
   client.on("connection", () =>
     console.log("Connected to redis server successfully")
