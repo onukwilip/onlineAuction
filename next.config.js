929f7e38c48836fe38e94b226afc2c4f8659c0ee
onukwilip <onukwilip@gmail.com>
2023-12-06 02:19:46 +0100

    Successfully converted the web app to a PWA

--- next.config.js
+++ next.config.js
@@ -3,6 +3,10 @@
 const withPWA = require("next-pwa")({
   dest: "public",
   register: true,
+  fallbacks: {
+    document: "/_offline",
+    image: "/offline.png",
+  },
   // disable: process.env.NODE_ENV !== "production",
 });
 
